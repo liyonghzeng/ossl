0c09e2b8d98c1f8588699a3695dfc8d355fad010
Your Name <liyonghzeng@2548722906@qq.com>
2019-07-16 11:55:50 +0000

    debug

--- app/Http/Controllers/OutoloadController.php
+++ app/Http/Controllers/OutoloadController.php
@@ -10,6 +10,6 @@ class OutoloadController extends Controller
     public function index()
     {
        $ccc= file_get_contents("php://input");
-       file_put_contents("/log/a.log",$ccc,FILE_APPEND);
+       file_put_contents("log/a.log",$ccc,FILE_APPEND);
     }
 }
